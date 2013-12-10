fdb44506839ea0837d416a01a0322bdc81e600a8
lizequn <lzq910123@gmail.com>
2013-12-10 18:22:28 +0000

    change to key

---
+++ src/main/java/uk/ac/ncl/cs/zequnli/cc/controller/JoinController.java
@@ -0,0 +1,12 @@
+package uk.ac.ncl.cs.zequnli.cc.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Auther: Li Zequn
+ * Date: 10/12/13
+ */
+@Controller
+public class JoinController {
+
+}
